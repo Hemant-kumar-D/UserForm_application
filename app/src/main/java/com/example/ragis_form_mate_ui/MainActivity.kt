77abc176008c84420d6b90a2fee2c04b0a5fc40a
Hemant kumar <hemantkumar42852@gmail.com>
2023-03-03 14:37:46 +0530

    frantdisign commit

---
+++ app/src/main/java/com/example/ragis_form_mate_ui/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ragis_form_mate_ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
